95a2374a1ae748b80f32f9392ce5519dc5717455
Awoniyi Oyindamola <damolaoyin005@gmail.com>
2025-05-26 14:38:49 +0100

    commit

--- src/Components/Randomusers.jsx
+++ src/Components/Randomusers.jsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 const Randomusers = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [hoveredCard, setHoveredCard] = useState(null);
+
 
   const fetchUsers = async () => {
     setLoading(true);
@@ -29,7 +31,12 @@ const Randomusers = () => {
       ) : (
         <div style={{ display: 'flex', flexWrap: 'wrap', gap: '24px' }}>
           {users.map((user, index) => (
-            <div key={index} style={{ border: '0.5px solid darkblue', padding: '16px', borderRadius: '10px',  backgroundColor: 'white', minWidth: '250px', gap: '8px', boxShadow: '-moz-initial'}}>
+            <div  key={index} style={{ border: '0.5px solid darkblue', padding: '16px', borderRadius: '10px',  backgroundColor: 'white', minWidth: '250px', gap: '8px', transition:'box-shadow o.3s ease',
+              boxShadow:
+              hoveredCard === index ? '0 8px 16px rgba(0,0,0,0.2)'
+                : '0 2px 6px rgba(0,0,0,0.1)',
+            }} onMouseEnter={() => setHoveredCard(index)}
+          onMouseLeave={() => setHoveredCard(null)} >
               <img style={{borderRadius: '8px' , justifySelf: 'center'}}  src={user.picture.medium} alt="User" />
               <h2 style={{fontSize: '18px', fontWeight: '600',  color: 'darkblue' , justifySelf: 'center'}}>{user.name.first} {user.name.last}</h2>
               <p style={{fontSize: '14px', fontWeight: '600',  color: '#555555' , justifySelf: 'center'}}> Email: {user.email}</p>
